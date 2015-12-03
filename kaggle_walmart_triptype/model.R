4d9463cb08f3a5475f8f6e0a42527641dca8d4d8
Bikash <er.bikash21@gmail.com>
2015-12-03 17:58:38 -0500

    read the parameter.

--- kaggle_walmart_triptype/model.R
+++ kaggle_walmart_triptype/model.R
@@ -37,11 +37,33 @@ write.csv(data, file="train_result.csv", quote=FALSE,row.names=FALSE)
 
 
 ####
-train.fet <- read.table("data/train_fet.csv",sep=',',header = T)
-test.fet <- read.table("data/test_fet.csv",sep=',',header = T)
+train <- read.table("data/train_result.csv",sep=',',header = T)
+test <- read.table("data/test_result.csv",sep=',',header = T)
 
 ## load train result <visitnumer, triptype>
-train_triptype <- read.table("data/train_triptype.csv",sep=',',header = T)
+train_triptype <- read.table("data/train_label.csv",sep=',',header = T)
 
 
+###
+dtrain <- xgb.DMatrix(train, label = train_triptype)
+dtest <- xgb.DMatrix(test)
+
+# xgboost parameters
+param <- list("objective" = "multi:softprob",    # multiclass classification 
+              "num_class" = num.class,    # number of classes 
+              "eval_metric" = "mlogloss",    # evaluation metric 
+              "nthread" = 8,   # number of threads to be used 
+              "max_depth" = 16,    # maximum depth of tree 
+              "eta" = 0.3,    # step size shrinkage 
+              "gamma" = 0,    # minimum loss reduction 
+              "subsample" = 1,    # part of data instances to grow tree 
+              "colsample_bytree" = 1,  # subsample ratio of columns when constructing each tree 
+              "min_child_weight" = 12  # minimum sum of instance weight needed in a child 
+)
+
+bst <- xgb.train( param, dtrain, 1 )
+# Note: we need the margin value instead of transformed prediction in set_base_margin
+# do predict with output_margin=TRUE, will always give you margin values before logistic transformation
+ptrain <- predict(bst, dtrain, outputmargin=TRUE)
+ptest  <- predict(bst, dtest, outputmargin=TRUE)
 
